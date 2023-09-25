65debf33e4fa03773879cde08e6d162018b5eb10
Luxe7 <luxe7@foxmail.com>
2023-09-25 21:28:05 +0800

    Day4 Middleware

--- main.go
+++ main.go
@@ -7,9 +7,32 @@ import (
 
 func main() {
 	r := glxe.New()
-	r.GET("/", func(c *glxe.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	r.GET("/index", func(c *glxe.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *glxe.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello glxe</h1>")
+		})
 
+		v1.GET("/hello", func(c *glxe.Context) {
+			// expect /hello?name=glxektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *glxe.Context) {
+			// expect /hello/glxektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *glxe.Context) {
+			c.JSON(http.StatusOK, glxe.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
 	r.Run(":9999")
 }
